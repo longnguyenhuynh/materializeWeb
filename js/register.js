1454dc5aac7fa7b1246117337cb6189b857a0083
Nguyen Huynh Long <phu.nguyenpfoem@gmail.com>
2019-06-26 12:52:20 +0700

    ok

--- js/register.js
+++ js/register.js
@@ -63,6 +63,7 @@ submitElement.addEventListener("click", submitAccount);
     }
   obj.send(data);
   console.log(data);
+  alert();
 }
 function receiveCheckingUserName() {
   let obj = new XMLHttpRequest();
